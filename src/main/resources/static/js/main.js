77b381babdeda25768333ad45c918ee5aa57b0e0
bonde <bond.elena.15.05@gmail.com>
2021-07-08 09:54:57 +0300

    Vue.js - базовый JavaScript интерфейс для REST приложения (часть 1).
    Знакомство с Vue.js документацией.
    Создали статический html файл и подключили к нему приложение на js.
    Файл js пишем покомпонентно.
    Знакомимся с props компонента и циклом. Помещаем данные из data в template.
    Для асинхронной работы с сервером мы возьмём vue resource и опишем объект (возвращает Promise из всех методов взаимодействия с сервером), позволяющий нам без лишнего кода делать простые запросы на получение и изменение данных
    Vue.resource и хуки жизненного цикла.
    then() извлекает данные из объекта Promise.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,22 +1,105 @@
-var messageApi = Vue.resource('/message{/id}'); //url по которому мы обращаемся к серверу
+function getIndex(list, id) {
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
 
+var messageApi = Vue.resource('/message{/id}'); //url по которому мы обращаемся к серверу
 // Определяем компоненты до создания Vue
  Vue.component('message-row', { // делаем каждую строку из data отдельным компонетом
-     props: ['message'],
-     template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+     props: ['message', 'editMethod', 'messages'],
+     template: '<div>' +
+         '<i>({{ message.id }})</i> {{ message.text }}' +
+         '<span style="position: absolute; right: 0">' +
+            '<input type="button" value="Edit" @click="edit" />' +
+            '<input type="button" value="x" @click="del" />' +
+         '</span>' +
+         '</div>',
+     methods: {
+         edit: function () { // передаем сообщение в форму редактирования
+             this.editMethod(this.message);
+         },
+         del: function () {
+             messageApi.remove({id: this.message.id}).then(result =>{
+                 if (result.ok) { //http status 200
+                     this.messages.splice(this.messages.indexOf(this.message), 1)
+                 }
+             })
+         }
+     }
+ });
+
+ // форма
+ Vue.component('message-form', {
+     props: ['messages', 'messageAttr'],
+     // во всех компонентах данные (data) не объект - а функция, которая возвращает объект.
+     data: function () {
+         return {
+             text: '', // данные только что введённого сообщения в форму
+             id: ''
+         }
+     },
+     watch: {
+         messageAttr: function (newVal) {
+             this.text = newVal.text;
+             this.id = newVal.id;
+         }
+     },
+     template: '<div>' + // в rest вместо <form> можно писать <div> в темплейте
+         '<input type="text" placeholder="Write something" v-model="text" />' +
+         '<input type="button" value="Save" @click="save" />' + // @ - обработка DOM-событий
+         '</div>',
+     methods: {
+         save: function () { // реализация того КАК будет сохранятся сообщение
+             var message = {text: this.text};
+             if (this.id) {
+                 messageApi.update({id: this.id}, message).then(result => // берем id из пути
+                    result.json().then(data => {
+                        var  index = getIndex(this.messages, data.id)
+                        this.messages.splice(index, 1, data);
+                        this.text = ''
+                        this.id = ''
+                    })
+                 )
+             } else {
+                 messageApi.save({}, message).then(result => // сохраняем сообщение в базу
+                     result.json().then(data => { // и тут же возвращаем это сообщение вместе с сгенерированным id.
+                         this.messages.push(data);
+                         this.text = ''
+                     })
+                 )
+             }
+         }
+     }
  });
 
 Vue.component('messages-list', {
-    props: ['messages'], // данные из data
-    template: '<div>' +
-        '<message-row v-for="message in messages" :key="message.id" :message="message"/>' +
+    props: ['messages'], // данные (data) из app
+    data: function () { // объект для хранения текущего сообщения из "message-row"
+        return {
+            message: null
+        }
+    },
+    template: '<div style="position: relative; width: 300px;">' +
+        // отображение формы на экране
+        '<message-form :messages="messages" :messageAttr="message" />' + // привязываем текущее сообщение к props формы
+        '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
+        ':editMethod="editMethod" :messages="messages"/>' +
         '</div>', //циклом выводим список из props построково, цикл работает только внутри контейнера
-    created: function() { //все верхнеуровневые функции не стрелочные, а анонимные
+    created: function () { //все верхнеуровневые функции (в каждом компоненте тоже) не стрелочные, а анонимные
         messageApi.get().then(result => // получаем поток данных из базы
             result.json().then(data => //получаем данные из json()
                 data.forEach(message => this.messages.push(message)) //помещаем данные в props
             )
         )
+    },
+    methods: {
+        editMethod: function (message) { // текущее сообщение помещаем в data компонента
+            this.message = message;
+        }
     }
 });
 
