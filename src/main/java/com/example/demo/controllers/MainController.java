82653bd5c6e5039b8b95c2d79960d1bf5d1b86ae
bonde <bond.elena.15.05@gmail.com>
2021-08-03 14:48:04 +0300

    Настраиваем Webpack и разбиваем фронт на модули.
    Устанавливаем node js (npm уже внутри) и Yarn. Настраиваем зависимости с помощью yarn через консоль. Формируются package.json и yarn.lock файлы. Добавляем конфигурационный файл для webpack (берем готовый вариант на канале). Правим скрипт запуска в json файле согласно версии webpack 5.
      Добавляем точку входа в index.html в зависимости от верссии продукта - продакшн или девелоп.
     Переменную версии продукта вносим в конфиг идеи. В дальнейшем это пригодится для работы с разными application.properties.
    Разбиваем main.js на файлы. В каждом vue компоненте есть темплейт (разметка), скрипт и стайл. Подключаем эти компоненты в App.vue.
    В связи с тем, что frontend приложения разнесён по модулям, переконфигурируем Vue Resource. Используем его по месту.

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,9 @@ public class MainController {
 
     private final MessageRepo messageRepo;
 
+    @Value("${spring.profiles.active}") // запрашиваем проперти из окружения
+    private String profile;
+
     @Autowired
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
@@ -25,11 +29,12 @@ public class MainController {
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal OAuth2User user) { //в параметрах мы ожидаем модель, чтобы заполнить её в контроллере
         HashMap<Object, Object> data = new HashMap<>();
-        if(user!=null) { // спрячет глобальную переменную от неавторизованного пользователя
+        if (user != null) { // спрячет глобальную переменную от неавторизованного пользователя
             data.put("usName", user.getAttribute("name")); // из OAuth2User берем только имя, чтоб не отправлять всю инфу из profile соц.сети на фронт
             data.put("messages", messageRepo.findAll());
         }
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
         return "index"; // т.к. это обычный контроллер - мы возвращаем имя шаблона.
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,126 +1,15 @@
+// благодаря babel можем писать тут без ";" и сокращать синтаксис фунуций
+import Vue from 'vue'
 
+// в ковычках пишем имя из json файла
+import VueResource from 'vue-resource'
 
-function getIndex(list, id) {
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].id === id) {
-            return i;
-        }
-    }
-    return -1;
-}
+// воспринимает static.js как корневой т.к. указали модули в файле webpack.config.js
+import App from 'pages/App.vue'
 
-var messageApi = Vue.resource('/message{/id}'); //url по которому мы обращаемся к серверу
-// Определяем компоненты до создания Vue
+Vue.use(VueResource)
 
- // форма
- Vue.component('message-form', {
-     props: ['messages', 'messageAttr'],
-     // во всех компонентах данные (data) не объект - а функция, которая возвращает объект.
-     data: function () {
-         return {
-             text: '', // данные только что введённого сообщения в форму
-             id: ''
-         }
-     },
-     watch: {
-         messageAttr: function (newVal, oldVal) {
-             this.text = newVal.text;
-             this.id = newVal.id;
-         }
-     },
-     template: '<div>' + // в rest вместо <form> можно писать <div> в темплейте
-         '<input type="text" placeholder="Write something" v-model="text" />' + //атрибут v-model передает данные отсюда в data
-         '<input type="button" value="Save" @click="save" />' + // @ - обработка DOM-событий
-         '</div>',
-     methods: {
-         save: function () { // реализация того КАК будет сохранятся сообщение
-             var message = {text: this.text};
-             if (this.id) {
-                 messageApi.update({id: this.id}, message).then(result => // берем id из пути
-                    result.json().then(data => {
-                        var  index = getIndex(this.messages, data.id)
-                        this.messages.splice(index, 1, data);
-                        this.text = '' //отчищаем форму
-                        this.id = ''
-                    })
-                 )
-             } else {
-                 messageApi.save({}, message).then(result => // сохраняем сообщение в базу
-                     result.json().then(data => { // .save() тут же возвращает это сообщение вместе с сгенерированным id.
-                         this.messages.push(data); // помещаем в отображаемый "список"
-                         this.text = ''
-                     })
-                 )
-             }
-         }
-     }
- });
-
-Vue.component('message-row', { // делаем каждую строку из data отдельным компонетом
-    props: ['message', 'editMethod', 'messages'],
-    template: '<div>' +
-        '<i>({{ message.id }})</i> {{ message.text }}' +
-        '<span style="position: absolute; right: 0">' +
-        '<input type="button" value="Edit" @click="edit" />' +
-        '<input type="button" value="x" @click="del" />' +
-        '</span>' +
-        '</div>',
-    methods: {
-        edit: function () { // передаем сообщение в форму редактирования
-            this.editMethod(this.message);
-        },
-        del: function () {
-            messageApi.remove({id: this.message.id}).then(result =>{
-                if (result.ok) { //http status 200
-                    this.messages.splice(this.messages.indexOf(this.message), 1)
-                }
-            })
-        }
-    }
-});
-
-Vue.component('messages-list', {
-    props: ['messages'], // данные (data) из app
-    data: function () { // объект для хранения текущего сообщения из "message-row"
-        return {
-            message: null
-        }
-    },
-    template: '<div style="position: relative; width: 300px;">' +
-        // отображение формы на экране
-        '<message-form :messages="messages" :messageAttr="message" />' + // привязываем текущее сообщение к props формы
-        '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
-        ':editMethod="editMethod" :messages="messages" />' +
-        '</div>', //циклом выводим список из props построково, цикл работает только внутри контейнера
-    methods: {
-        editMethod: function (message) { // текущее сообщение помещаем в data компонента
-            this.message = message;
-        }
-    }
-});
-
-var app = new Vue({
+new Vue({
     el: '#app',
-    template:
-        '<div>' +
-            '<div v-if="!usName">Необходимо авторизоваться через' +
-                ' <a href="/login">Google или Github</a>' +
-            ' </div>' +
-            '<div v-else>' +
-                '<div>{{usName}}&nbsp<a href="/logout">Выйти</a></div>' +
-                //название компонента помещаем в тег и в ковычки
-                '<messages-list :messages="messages" />' + // в свойствах указываем props
-            '</div>' +
-        '</div>',
-    data: { //эти данные можно обновлять только через методы массивов (push, splice, delete)
-        messages: frontendData.messages,
-        usName: frontendData.usName
-    }, // инициализацию коллекции messages переносим на уровень выше
-    created: function () { //все верхнеуровневые функции (в каждом компоненте тоже) не стрелочные, а анонимные
-        // messageApi.get().then(result => // получаем поток данных из базы
-        //     result.json().then(data => //получаем данные из json()
-        //         data.forEach(message => this.messages.push(message)) //помещаем данные в props
-        //     )
-        // )
-    },
-});
+    render: a => a(App)
+})
